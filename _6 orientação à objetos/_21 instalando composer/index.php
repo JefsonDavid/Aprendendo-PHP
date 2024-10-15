d52480831d3c45e0a8e200e9789d8c7d64a6424e
Jéfson David🖖😎 <jefsondavid@gmail.com>
2024-10-15 16:07:57 -0300

    Add files via upload

---
+++ _6 orientação à objetos/_21 instalando composer/index.php
@@ -0,0 +1,9 @@
+<?php   
+    //Existe duas formas de instalar o composer no nosso pc.
+        
+        //WINDOWS: Vc vai entrar no site https://getcomposer.org/
+            //-> Vc vai em GETTING STARTED
+            //-> Depois vc vai na opção INSTALLATION - WINDOWS e clica nela
+            //-> Depois vc vai clicar em COMPOSER-SETUP.EXE
+                //O que esse arquivo vai fazer? Ele vai instalar o COMPOSER na sua máquina e vai SETAR o comando COMPOSER no PATH pra vc conseguir no seu terminal execultar o comando COMPOSER  e ele funcionar
+
